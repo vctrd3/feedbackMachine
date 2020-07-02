91292a7d27379a45b1c23b714b6a5e1ef3ac63c8
Victor <vctrd3@gmail.com>
2020-07-01 20:22:40 -0400

    change test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({hi:'there'})
+  res.send({test:'one'})
 });
 
 const PORT = process.env.PORT || 5000;
