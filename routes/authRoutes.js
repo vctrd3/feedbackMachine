95f134d15c5471baeab6122539bc43e69c1b5824
Victor <vctrd3@gmail.com>
2020-07-02 02:19:54 -0400

    done with google oauth

---
+++ routes/authRoutes.js
@@ -0,0 +1,21 @@
+const passport = require('passport');
+const router = require('express').Router();
+
+router.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+  })
+);
+
+router.get('/auth/google/callback', passport.authenticate('google'));
+
+router.get('/api/logout', (req, res) => {
+  req.logout();
+  res.send(req.user);
+});
+
+router.get('/api/current_user', (req, res) => {
+  res.send(req.user);
+});
+
+
+module.exports = router;
---
+++ services/passport.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => { //user is User model existingUser or newUser
+  done(null, user.id); //user.id is from user model on mongodb
+})
+
+passport.deserializeUser(async (id, done) => {
+  try{
+    const user = await User.findById(id);
+    done(null, user);
+  } catch{
+  }
+});
+
+passport.use(new GoogleStrategy(
+  {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, 
+  async (accessToken, refreshToken, profile, done) => {
+    try{
+      const existingUser = await User.findOne({googleId: profile.id});
+      if(existingUser){
+        done(null, existingUser); //first arg is for error object, second is user to return
+    } else{
+        const newUser = await new User({ googleId: profile.id}).save();
+        done(null, newUser);
+    }
+    } catch{
+    }
+  })
+);
