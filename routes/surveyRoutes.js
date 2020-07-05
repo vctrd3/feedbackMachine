89c74f174a71379814e7c04d98cdef3b75fdf429
Victor <vctrd3@gmail.com>
2020-07-04 23:38:04 -0400

    set sendgrid webhook

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const router = require('express').Router();
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
@@ -7,9 +10,50 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 
 const Survey = mongoose.model('surveys');
 
+router.get('/api/surveys', requireLogin, async (req, res) => {
+  const surveys = await Survey.find({ _user: req.user.id })
+    .select({ recipients: false});
+
+  res.send(surveys);
+});
+
+
 router.get('/api/surveys/:surveyId/:choice', (req, res) => {
   res.send('Thanks for voting!');
 });
+
+router.post('/api/surveys/webhooks', (req, res) => {
+  const p = new Path('/api/surveys/:surveyId/:choice');
+
+  _.chain(req.body)
+    .map(({email, url}) => {
+      const match = p.test(new URL(url).pathname);
+      if(match){
+        return { email, surveyId: match.surveyId, choice: match.choice };
+      }
+    })
+    //compact remove duplicate undefined records
+    .compact()
+    //remove duplicate records by email and surveyId
+    .uniqBy('email', 'surveyId')
+    .each(({ surveyId, email, choice }) => {
+      Survey.updateOne(
+      {
+        _id: surveyId,
+        recipients: {
+          $elemMatch: {email:email, responded: false}
+        }
+      }, {
+        $inc: { [choice]: 1 },
+        $set: { 'recipients.$.responded': true },
+        lastResponded: new Date()
+      }).exec();
+    })
+    .value();
+
+  res.send({});
+});
+
 
 router.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
   const { title, body, subject, recipients } = req.body;
